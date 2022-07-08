9f837390d92f6cd6a598e31b38d37c62a94ffbc3
D. Ford <105244385+CodingBuck@users.noreply.github.com>
2022-07-08 06:47:54 -0500

    Add files via upload

---
+++ Binary Search.py
@@ -0,0 +1,59 @@
+import random
+import time
+# implementation of binary search algorithm
+
+#Proving that binary search is faster than naive search!
+
+
+def naive_search(l, target):
+
+    for i in range(len(l)): # example l = [1,3,10,12]
+        if l[i] == target:
+            return 1
+    return -1
+
+#binary search uses divide and conquer!
+
+def binary_search(l, target, low=None, high=None):
+      if low is None:
+        low = 0
+      if high is None:
+          high = len(l) -1
+
+      if high < low:
+          return -1
+
+
+      midpoint = (len(l)) // 2
+
+      if l[midpoint] ==target:
+          return midpoint
+      elif target < l[midpoint]:
+          return binary_search(l, target, low, midpoint-1)
+      else:
+          # target > l[midpoint]
+          return binary_search(l, target, midpoint+1, high)
+
+      if __name__ == '__main__':
+          #l = [1, 3, 5, 10,12]
+          #target = 10
+          #print(naive_search(l, target))
+          #print(binary_search(l, target))
+
+          length = 10000
+          sorted_list = set()
+          while len(sorted_list) < length:
+              sorted_list.add(random.randint(-3*length))
+          sorted_list = sorted(list(sorted_list))
+
+          start = time.time()
+          for target in sorted_list:
+              naive_search(sorted_list, traget)
+          end = time.time()
+          print("Naive search time: ", (end - start)/length, "seconds")
+
+          start = time.time()
+          for target in sorted_list:
+              binary_search(sorted_list, traget)
+          end = time.time()
+          print("Binary search time: ", (end - start)/length, "seconds")
